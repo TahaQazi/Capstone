d821d6bdd20f60afe366d0e3013a137b93ee1cb9
= <=>
2023-09-19 13:37:11 -0400

    models and Admin

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,8 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
-
+    def __str__(self):
+        return self.name 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
@@ -19,7 +23,21 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey('CarMake', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    dealer_id = models.IntegerField()
+    TYPE_CHOICES = [
+        ('Sedan', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'WAGON'),
+        ('Truck', 'Truck'),
+    ]
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    year = models.DateField()
 
+    def __str__(self):
+        return   str(self.car_make) + " " + self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
